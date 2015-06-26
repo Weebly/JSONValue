4c8b9d90f15250d4373abccf5c3d5f41167cc354
James Richard <james@weebly.com>
2015-06-26 07:25:24 -0700

    Initial Commit

---
+++ JSONValue/JSONValue.h
@@ -0,0 +1,19 @@
+//
+//  JSONValue.h
+//  JSONValue
+//
+//  Created by James Richard on 6/26/15.
+//  Copyright © 2015 Weebly. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for JSONValue.
+FOUNDATION_EXPORT double JSONValueVersionNumber;
+
+//! Project version string for JSONValue.
+FOUNDATION_EXPORT const unsigned char JSONValueVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <JSONValue/PublicHeader.h>
+
+
